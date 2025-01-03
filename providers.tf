f5d0678c66a5197804619fced22e55c885d6455d
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2025-01-03 20:57:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -14,7 +14,7 @@ cloud {
 
 provider "aws" {
   access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = 4Il+js4mEykKy68C0eVQEBefh0iQOmG25BjFY2Ts
+  secret_key = "4Il+js4mEykKy68C0eVQEBefh0iQOmG25BjFY2Ts"
   region  = "us-east-1"
   
 }
