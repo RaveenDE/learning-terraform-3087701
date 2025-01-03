a5e7384436f798e72d0e8caff7bccc86ed0f0c11
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2025-01-03 19:39:06 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,11 +8,12 @@ cloud {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>  3.74.2"
     }
   }
 }
 
 provider "aws" {
   region  = "us-west-2"
+  access_key = "AWS_ACCESS_KEY_ID "
+  secret_key = "AWS_SECRET_ACCESS_KEY"
 }
